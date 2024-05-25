1146a6daf754abb95cbf10ffba6801c49a44337a
unknown <lukotas100@gmail.com>
2024-05-25 20:40:33 -0300

    Arreglado bug del filtro por generacion y codigo mas pulido

--- js/main.js
+++ js/main.js
@@ -6,30 +6,18 @@ const arrowButton = document.getElementById("arrow-button");
 const advanceSearch = document.querySelector(".inferior-row");
 const buttons = document.querySelectorAll(".types");
 const gens = document.querySelectorAll(".gen");
-
 const returnButton = document.querySelector(".return-button")
 const mainContainer = document.getElementById("main-container")
-
 const inputSearch = document.getElementById("search-poke")
 
 
-// const isMobileDevice = () => {
-//     return /iPhone|iPad|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-// }
-
-
-// setTimeout(() =>{
-//     document.querySelector(".preloader").style.display = "none";
-//     document.querySelector(".main-container").style.display = "block";
-// }, isMobileDevice == true ? 3500 : 1500 )
-
-
-
+// funcion que muestra u oculta el modal
 const toggleMenu = () => {
     modal.classList.toggle("visible");
     menuLateral.classList.toggle("show-menu");
 }
 
+// funcion que muestra u oculta la busqueda avanzada
 const toggleAdvanceSearch = () => {
     advanceSearch.classList.toggle("show");
 }
@@ -86,28 +74,35 @@ const selectType= (tipo) =>{
     }
 }
 
+//funciones que estilizan los botones al darles click
 
-document.addEventListener("DOMContentLoaded", stylizeButtons);
-
-
-
-arrowButton.addEventListener("click", toggleAdvanceSearch);
-btnOpenMenu.addEventListener("click", toggleMenu);
-btnCloseMenu.addEventListener("click", toggleMenu);
+const removeTypeSelected = () =>{
+    buttons.forEach((button) => {
+        button.classList.remove("selected-type")
+    });
+}
 
 buttons.forEach(button =>{
     button.addEventListener("click", () =>{
-        button.classList.toggle("unselected-type")
+        removeTypeSelected();
+        button.classList.add("selected-type");
     })
 });
+
+const removeGenSelected = () =>{
+    gens.forEach((gen) => {
+        gen.classList.remove("selected-gen");
+    });
+}
 
 gens.forEach(gen =>{
     gen.addEventListener("click", () =>{
-        gen.classList.toggle("unselected-gen")
+        removeGenSelected();
+        gen.classList.add("selected-gen") 
     })
 });
 
-
+// funcion para el boton que te devuelve al principio de la pagina con una animacion
 returnButton.addEventListener("click", ()=> {
     mainContainer.scrollIntoView({behavior: "smooth"})
 })
@@ -119,16 +114,10 @@ inputSearch.addEventListener("input", () =>{
 
     const cards = document.querySelectorAll(".pokemon-card-background");
 
-    cards.forEach((card) => {
-        
+    cards.forEach((card) => { 
         let pokeName = card.firstElementChild.lastElementChild.children[1].textContent
         
-        if (!pokeName.includes(getInput)) {
-            card.style.display = "none";
-        }else{
-            card.style.display = "block";
-        }
-
+        card.style.display = pokeName.includes(getInput) ? "block" : "none";
     });
 });
 
@@ -144,55 +133,37 @@ const genRange = {
     KALOS: [650,721],
     ALOLA: [722,809],
     GALAR: [810,905],
-    PALDEA: [906,1025]
+    PALDEA: [906,1025],
+    TODAS: [1,1025]
 }
 
-const disguiseByRange = (start, end) =>{
+const showByGen = (start, end) => {
     const cards = document.querySelectorAll(".pokemon-card-background");
 
-    cards.forEach((card) =>{
-        
-        console.log(start)
-        console.log(end)
-        
-        getId = card.firstElementChild.lastElementChild.children[0].innerHTML.substring(1);
-        if ( getId >= start && getId <= end ){ 
-            card.style.display = "none";
-        }
-    });
-} 
+    console.log(start);
+    console.log(end);
 
-const showCard = (start, end) =>{
-    const cards = document.querySelectorAll(".pokemon-card-background");
-
-    cards.forEach((card) =>{
-        
-        console.log(start)
-        console.log(end)
-        
+    cards.forEach((card) => {
         getId = card.firstElementChild.lastElementChild.children[0].innerHTML.substring(1);
-        if ( getId >= start && getId <= end ){ 
-            card.style.display = "block";
-        }
+
+        card.style.display = (getId >= start && getId <= end) ? "block" : "none";
     });
 }
 
-gens.forEach((gen) => {
+gens.forEach((gen) =>{
     gen.addEventListener("click", () =>{
-        if(gen.classList.contains("unselected-gen")){
-            const getRange = genRange[gen.textContent]
-            disguiseByRange(getRange[0],getRange[1])
-        }else{
-            if (!gen.classList.contains("unselected-gen")) {
-                const getRange = genRange[gen.textContent]
-                showCard(getRange[0],getRange[1])
-            }
+        if(gen.classList.contains("selected-gen")){
+            const getRange = genRange[gen.textContent];
+            showByGen(getRange[0], getRange[1]);
         }
     });
 });
 
-
-
+//eventos iniciales
+document.addEventListener("DOMContentLoaded", stylizeButtons);
+arrowButton.addEventListener("click", toggleAdvanceSearch);
+btnOpenMenu.addEventListener("click", toggleMenu);
+btnCloseMenu.addEventListener("click", toggleMenu);
 
 /*
 
@@ -200,11 +171,7 @@ TO-DO :
 
 -Crear funcion para filtrar por tipos
 
--Solucionar bug filtro gen cuando se busca un pokemon de una region ocultada con el filtro, este aparece y cuando se borra todo lo del input aparece toda la pokedex
-
--Cambiar filtro de generacion, en vez de ocultar, que solo muestre esa region
-CREAR NUEVOS JS PARA:
-
 -Agregar animacion de cuando aparezca en el viewport
+
 */
 
