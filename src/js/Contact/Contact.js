17345f6c42efc8b25e619c27a8616bb15cdf1189
unknown <lukotas100@gmail.com>
2024-07-03 23:57:03 -0300

    add contact styles, missing logic for send emails and version tablet and desktop

--- src/js/Contact/Contact.js
+++ src/js/Contact/Contact.js
@@ -2,8 +2,7 @@ const modal = document.getElementById("modal-menu-container");
 const menuLateral = document.querySelector(".info-menu-container");
 const btnOpenMenu = document.getElementById("btn-menu");
 const btnCloseMenu = document.getElementById("btn-close");
-
-
+const returButton = document.getElementById("return-button")
 
 const toggleMenu = () => {
     modal.classList.toggle("visible");
@@ -11,9 +10,11 @@ const toggleMenu = () => {
     document.body.style.overflow = (modal.classList.contains("visible")) ? "hidden": "scroll";
 }
 
-document.querySelector(".return-button").addEventListener("click", ()=> {
+returButton.addEventListener("click", ()=> {
     document.querySelector("header").scrollIntoView({behavior: "smooth"});
 });
 
 btnOpenMenu.addEventListener("click", toggleMenu);
 btnCloseMenu.addEventListener("click", toggleMenu);
+
+// Falta agregar la logica del mail
