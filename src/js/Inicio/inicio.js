07cdfc9994d7298d6ff6ebad2fffc68a837f3356
unknown <lukotas100@gmail.com>
2024-07-05 19:42:49 -0300

    Finish pokedex version 1.0

--- src/js/Inicio/inicio.js
+++ src/js/Inicio/inicio.js
@@ -3,6 +3,7 @@ const menuLateral = document.querySelector(".info-menu-container");
 const btnOpenMenu = document.getElementById("btn-menu");
 const btnCloseMenu = document.getElementById("btn-close");
 const container = document.getElementById("random-pokemon-section");
+const searchButton = document.querySelector(".search-button")
 
 const toggleMenu = () => {
     modal.classList.toggle("visible");
@@ -34,8 +35,7 @@ const initializeAnimations = () =>{
     sr.reveal(".random-pokemon-section", {
         duration: 1500,
         origin: "rigth",
-        distance: "-100px",
-        reset: true
+        distance: "-100px"
     });
 }
 
@@ -43,16 +43,17 @@ const generateRandomNumber = () =>{
     return Math.floor(Math.random() * 1025);
 }
 
-const generateRandomPokemon = async () => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${generateRandomNumber()}`)
+const generatePokemon = async (numberRandom) => {
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${numberRandom}`)
+        
+        const poke = await response.json();
+        
+        const pokeContainer = document.createElement("DIV");
 
-    const poke = await response.json();
+        pokeContainer.classList.add("random-pokemon-container");
 
-    const pokeContainer = document.createElement("DIV");
-
-    pokeContainer.classList.add("random-pokemon-container");
-
-    pokeContainer.innerHTML = `
+        pokeContainer.innerHTML = `
                 <div class="pokemon-image-container">
                      <img src="${poke.sprites.other["official-artwork"].front_default}" alt="pokemon">
                 </div>
@@ -67,23 +68,34 @@ const generateRandomPokemon = async () => {
                             <span class="second-type">${poke.types.length > 1 ? `<span>${poke.types[1].type.name.toUpperCase()}</span>` : ``}</span>
                         </div>
                         <div class="h-container">
-                            <span class="poke-heigth">HEIGTH: <b>${poke.height.toString().slice(0, -1) + "." + poke.height.toString().slice(-1)}M</b></span>
+
+                            <span class="poke-heigth">HEIGTH: ${poke.height >= 10 ? `<b>${poke.height.toString().slice(0, -1) + "." + poke.height.toString().slice(-1)}M</b>` : `<b>${poke.height.toString().slice(0, -1) + "0." + poke.height.toString().slice(-1)}M</b>`}</span>
                         </div>
                         <div class="w-container">
-                            <span class="poke-width">WEIGHT: <b>${poke.weight.toString().slice(0, -1) + "." + poke.height.toString().slice(-1)}KG</b></span>
+                            <span class="poke-width">WEIGHT: ${poke.weight >= 10 ? `<b>${poke.weight.toString().slice(0, -1) + "." + poke.weight.toString().slice(-1)}KG</b>` : `<b>${poke.weight.toString().slice(0, -1) + "0." + poke.weight.toString().slice(-1)}KG</b>`}</span>
                         </div>
                     </div>
                 </section> `
-    container.appendChild(pokeContainer);
+        container.appendChild(pokeContainer);
+    }catch{
+        throw new Error("Ingrese un pokemon valido")
+    }
 }
 
 document.querySelector(".return-button").addEventListener("click", ()=> {
     document.querySelector("header").scrollIntoView({behavior: "smooth"});
 })
 
+searchButton.addEventListener("click", () => {
+    const pokemon = document.querySelector(".search-input");
+    container.innerHTML = "";
+    generatePokemon(pokemon.value.toLowerCase());
+    setTimeout(() => {
+        document.querySelector(".random-pokemon-section").scrollIntoView({behavior: "smooth"});
+    }, 100);
+});
+
 btnOpenMenu.addEventListener("click", toggleMenu);
 btnCloseMenu.addEventListener("click", toggleMenu);
 document.addEventListener("DOMContentLoaded", initializeAnimations);
-document.addEventListener("DOMContentLoaded", generateRandomPokemon);
-
-//Falta agregar la funcionalidad de que al buscar un pokemon abajo salgan los datos 
+document.addEventListener("DOMContentLoaded", generatePokemon(generateRandomNumber()));
